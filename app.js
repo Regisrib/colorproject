53de6633da38573ac94fe8a39f7f3246e76a61bd
Regisrib <155403194+Regisrib@users.noreply.github.com>
2025-07-17 20:46:02 -0300

    criaçao de js hexa decimal

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025']
 
 const btn = document.getElementById('btn')
 const color = document.querySelector('.span-color')
@@ -9,10 +9,9 @@ btn.addEventListener('click', function(){
     console.log('clicou no botão')
     const randomNumber = getRandomNumbers()
                     //background-color
-    const mainColor = document.querySelector('main-color')
-    mainColor.computedStyleMap.backgroundColor = [randomNumber]
-    // document.body.style.backgroundColor = colors[randomNumber]
-    // color.textContent = colors[randomNumber] 
+    const mainColor = document.querySelector('#main-color')
+    mainColor.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[getRandomNumbers()]
 })
 
 function getRandomNumbers() {
